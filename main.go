47bc3e1bbc93d7cf8a2d91b9bd082cce4d03c961
Mike Simons <mike@mikesimons.co.uk>
2016-09-05 15:55:13 +0100

    Swaps golang flag for urface cli for better control of output

--- main.go
+++ main.go
@@ -1,49 +1,87 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/mikesimons/earl"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	opts := &PacYakOpts{}
+	app := cli.NewApp()
+	app.Name = "pacyak"
+	app.Version = "1.0"
 
-	flag.StringVar(&opts.ListenAddr, "addr", "127.0.0.1:8080", "Proxy listen address")
-	flag.StringVar(&opts.PingCheckHost, "pinghost", "", "Availability check host")
-	flag.StringVar(&opts.PacProxy, "pacproxy", "", "Proxy for fetching PAC file")
-	flag.StringVar(&opts.LogLevelStr, "loglevel", "info", "Log level: debug, info, warn, error")
-	flag.Parse()
+	cli.AppHelpTemplate = `{{.Name}} version {{.Version}} - For the unfortunate souls stuck behind corporate proxies
 
-	tmp, err := logrus.ParseLevel(opts.LogLevelStr)
-	if err != nil {
-		fmt.Printf("Invalid log level '%s'. Valid levels are: debug, info, warn, error")
-		os.Exit(1)
+{{.HelpName}} [options] <pac location>
+
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}
+`
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "listen",
+			Usage: "Pacyak will listen for requests to this address",
+			Value: "127.0.0.1:8080",
+		},
+		cli.StringFlag{
+			Name:  "ping-host",
+			Usage: "Host only accessible from within your proxy. Required if PAC location is a file. (default: Host of PAC location)",
+		},
+		cli.StringFlag{
+			Name:  "pac-proxy",
+			Usage: "Proxy for pac file. (Only necessary if your PAC location requires a proxy to be set)",
+		},
+		cli.StringFlag{
+			Name:  "log-level",
+			Usage: "Log level (debug, info, warn, error)",
+			Value: "info",
+		},
 	}
-	opts.LogLevel = tmp
 
-	args := flag.Args()
+	app.Action = func(c *cli.Context) error {
+		opts := &PacYakOpts{}
 
-	if len(args) < 1 {
-		fmt.Printf("Usage: pacyak <pac-file>\n")
-		os.Exit(1)
-	}
-
-	opts.PacFile = args[0]
-
-	url := earl.Parse(opts.PingCheckHost)
-	if url.Host == "" {
-		url = earl.Parse(opts.PacFile)
-		if url.Host == "" {
-			fmt.Printf("-pinghost is required if pac-file is not a URL")
-			os.Exit(1)
+		tmp, err := logrus.ParseLevel(c.String("log-level"))
+		if err != nil {
+			return cli.NewExitError(fmt.Sprintf("Invalid log level '%s'. Valid levels are: debug, info, warn, error", tmp), 1)
 		}
+		opts.LogLevel = tmp
+
+		if c.NArg() < 1 {
+			cli.ShowAppHelp(c)
+			os.Exit(0)
+		}
+
+		opts.PacFile = c.Args().Get(0)
+
+		url := earl.Parse(opts.PacFile)
+		if url.Scheme == "" || url.Scheme == "file" {
+			if _, err := os.Stat(opts.PacFile); os.IsNotExist(err) {
+				return cli.NewExitError("PAC location is not a valid URL and file does not exist. If it is a URL, please specify a protocol (e.g. http://)", 1)
+			}
+		}
+
+		opts.PingCheckHost = earl.Parse(c.String("ping-host")).Host
+		if c.String("ping-host") == "" {
+			url = earl.Parse(opts.PacFile)
+			if url.Scheme == "" || url.Scheme == "file" {
+				return cli.NewExitError("--ping-host is required if PAC location is not a URL", 1)
+			}
+			opts.PingCheckHost = url.Host
+		}
+
+		opts.PacProxy = c.String("pac-proxy")
+		opts.ListenAddr = c.String("listen")
+
+		Run(opts)
+		return nil
 	}
 
-	opts.PingCheckHost = url.Host
-
-	Run(opts)
+	app.Run(os.Args)
 }
